3fccbaa242f58b91e15f46c205f38562159581b4
Ania pq <melanpq@gmail.com>
2023-02-09 04:35:44 -0500

    hook  personalizado para validar formulario

--- burger-queen/src/hooks/useForm.js
+++ burger-queen/src/hooks/useForm.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
-export const useForm = (initialForm) => {
+export const useForm = (initialForm,validateForm) => {
   const [form, setForm] = useState(initialForm)
-  const [error, setError] = useState(initialForm)
-  const [loading, setLoading] = useState(initialForm)
-   const [response,setResponse]=useState(initialForm)
-  
+  const [error, setError] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState(null)
+  const handleChange = (event) => { }
+  const handleBlur = (event) => { }
+  const handleSubmit = (event) => { }
+  return {
+    form, error, loading,response, handleChange, handleBlur, handleSubmit
+  }
 }
