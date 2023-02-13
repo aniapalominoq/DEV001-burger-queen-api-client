14f5a57fec6a6ae861c2be8c4c0ca96dd8024b3c
Erika Duarte <erika.duarte.mora@gmail.com>
2023-02-13 12:38:50 -0500

    router rutas publicas y privadas para iniciar sesion

--- burger-queen/src/pages/Login/Login.jsx
+++ burger-queen/src/pages/Login/Login.jsx
@@ -1,40 +1,59 @@
-import { useForm } from "../../hooks/useForm";
 import "./Login.css";
-import Loader  from "../../components/Loarder/Loader";
+// import Loader  from "../../components/Loarder/Loader";
+import PrivateRoute from "../../components/router/PrivateRoute";
+import { useAuthContext } from "../../Context/authContext";
+import { useState } from "react";
 
-const initialForm = {
-  email: "",
-  password: "",
-};
+const {isAuthenticated} = useAuthContext; 
 
-const validateForm = (form) => {
-  let errors = {};
 
-  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+// const validateForm = (form) => {
+//   let errors = {};
 
-  if (!form.email.trim()) {
-    errors.email = "El campo email es requerido";
-  } else if (!regexEmail.test(form.email.trim())) {
-    errors.email = "El campo 'email' es incorrecto";
-  }
-  if (!form.password.trim()) {
-    errors.password = "El campo 'password' es requerido";
-  }
+//   let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
 
-  return errors;
-};
+//   if (!form.email.trim()) {
+//     errors.email = "El campo email es requerido";
+//   } else if (!regexEmail.test(form.email.trim())) {
+//     errors.email = "El campo 'email' es incorrecto";
+//   }
+//   if (!form.password.trim()) {
+//     errors.password = "El campo 'password' es requerido";
+//   }
+
+//   return errors;
+// };
 
 const Login = () => {
-  const {
-    form,
-    errors,
-    loading,
-    response,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-  } = useForm(initialForm, validateForm);
+  const {peticion} = useAuthContext();
+  const [form, setForm] = useState('');
 
+  const handleChange = (event) => {
+    const { name, value } = event.target; //destructurar el objeto event.target
+    setForm({
+      ...form, //le pasamos una copia del estado del fomulario
+      [name]: value,
+    });
+  };
+
+  const handleBlur = (event) => {
+    handleChange(event);
+    // setErrors(validateForm(form)); //llegar con los errores en el formulario
+  };
+  const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event.target.email.value)
+        peticion(event.target.email.value, event.target.password.value)
+        // setErrors(validateForm(form)); //que no existan errores
+        // if (Object.keys(errors).length === 0) {
+        //   alert("enviando formulario");
+        //   setLoading(true);
+        
+          
+        // } else {
+        //   return ;
+        // }
+      };
   return (
     <section className="container">
       <form className="container-form" onSubmit={handleSubmit}>
@@ -59,7 +78,7 @@ const Login = () => {
               <i className="fas fa-check"></i>
             </span>
           </p>
-          {errors.email && <p className="help is-danger">{errors.email}</p>}
+          {/* {errors.email && <p className="help is-danger">{errors.email}</p>} */}
         </div>
         <div className="field">
           <label className="label">Password</label>
@@ -81,16 +100,17 @@ const Login = () => {
               <i className="fas fa-eye"></i>
             </span>
           </p>
-          {errors.password && (
-            <p className="help is-danger">{errors.password}</p>
-          )}
+          {/* {errors.password && (
+            {/* <p className="help is-danger">{errors.password}</p> */}
+          {/* )} */}
         </div>
         <div className="btn-control">
           <input className="button" type="submit" value="Sign In" />
         </div>
       </form>
-     { loading && <Loader/>}
-     {response && alert("Bienvenidos")}
+     {/* { loading && <Loader/>} */}
+     {isAuthenticated && <PrivateRoute/>}
+ 
     </section>
   );
 };
