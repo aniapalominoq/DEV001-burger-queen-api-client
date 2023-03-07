e760bcb132ff26cc57c86bd11bb74154d5970384
Ania pq <melanpq@gmail.com>
2023-03-06 21:03:26 -0500

    css module footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Footer.css";
+import style from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className="Footer">
+    <footer className={style.Footer}>
       <strong>Copyright © 2023</strong>
       <span>
         <strong>Desarrollado por</strong>
